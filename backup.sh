45763215a1de956992b3d33592b0df57060091e4
LT-209 <94721500+Luquistroll209@users.noreply.github.com>
2026-05-13 17:51:30 +0200

    cambios en el backup

--- backup.sh
+++ backup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Ruta donde está el script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Configuración
-ARCHIVO="/home/luquis/Documentos/GitHub/Learn.py/backend/backend/db.sqlite3"
-CARPETA="/home/luquis/Documentos/GitHub/Learn.py/backend/backend/media"
+ARCHIVO="$SCRIPT_DIR/backend/backend/db.sqlite3"
+CARPETA="$SCRIPT_DIR/backend/backend/media"
 DESTINO="/backupLearn.py"
 
 if [ "$EUID" -ne 0 ]; then
@@ -10,9 +13,9 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-if [ ! -d "$DESTINO" ]; then
-    mkdir "$DESTINO"
-fi
+# Crear carpeta backup si no existe
+mkdir -p "$DESTINO"
+
 # Fecha y hora
 FECHA=$(date +"%Y-%m-%d_%H-%M-%S")
 
